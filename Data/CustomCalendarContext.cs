1a73072ada3abff7eff9848ce8b5d8413b5f0ff9
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-21 12:09:02 -0300

    DBContext created

---
+++ Data/CustomCalendarContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomCalendar.Entities;
+
+namespace CustomCalendar.Data
+{
+    public class CustomCalendarDBContext : DbContext
+    {
+        public CustomCalendarDBContext(DbContextOptions<CustomCalendarDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Block> Block { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
