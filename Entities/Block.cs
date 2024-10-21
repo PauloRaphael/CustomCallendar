60f88311b7b994335c409f2b3361e2e9b4a2440f
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-21 12:08:43 -0300

    Entity created

---
+++ Entities/Block.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomCalendar.Entities
+{
+    public class Block
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string EventText { get; set; }
+    }
+}
