22fd4449115316232fb6d469d8d0b9fb02213a2c
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-24 20:31:00 -0300

    git status '

--- DataRepository/Entities/Block.cs
+++ DataRepository/Entities/Block.cs
@@ -1,9 +1,19 @@
-﻿namespace DataRepository.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataRepository.Entities;
 
 public class Block
 {
+    
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "{0} Required")]
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "{0} Required")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
     public DateTime Date { get; set; }
+
+    [Required(ErrorMessage = "{0} Required")]
     public string EventText { get; set; }
 }
