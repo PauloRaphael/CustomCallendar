beea7d293193c2d7a36fdcce3a0240686a862419
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-11-02 11:37:00 -0300

    Category service created

---
+++ DataRepository/Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using DataRepository.Data;
+using DataRepository.Entities;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+namespace DataRepository.Services
+{
+    public class CategoryService
+    {
+        private readonly CustomCalendarDBContext _context;
+
+        public CategoryService(CustomCalendarDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _context.Category.ToListAsync();
+        }
+
+        public async Task InsertAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(Category category)
+        {
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
